f1bd840cd64ba77906cd07af4672e324349df11f
Samsson56 <samssonravi@gmail.com>
2023-03-20 20:59:00 +0530

    Co-authored-by: Vignesh1702 <Vignesh1702@users.noreply.github.com>

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className='header__left'>
+            <img src="" alt='logo'/>
+            <h1>Web Music</h1>
+        </div>
+        
+        <h4>Account</h4>
+        <h4>Music Languages</h4>
+        <h4>Sign up</h4>
+        <h4>Login</h4>  
+    </div>
+  )
+}
+
+export default Header;
