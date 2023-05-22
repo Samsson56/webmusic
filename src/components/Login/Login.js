107fe673d5509389e85f21890eb41b47171e3165
Samsson R <samssonravi@gmail.com>
2023-05-22 11:08:04 +0530

    feat: add navigation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,194 +3,197 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "../../Styles/Login.css";
 import pic from "../../assets/logo.jpg";
+import {useNavigate} from "react-router-dom"
 
-const Login = () => {
-  const CLIENT_ID = "20811cb4942748bb987dea4e20eeeed9";
-  const REDIRECT_URI = "https://webmusic-alpha.vercel.app/music";
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-  const RESPONSE_TYPE = "token";
+// const Login = () => {
+//   const CLIENT_ID = "20811cb4942748bb987dea4e20eeeed9";
+//   const REDIRECT_URI = "https://webmusic-alpha.vercel.app/music";
+//   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+//   const RESPONSE_TYPE = "token";
 
-  const [token, setToken] = useState("");
-  const [searchKey, setSearchKey] = useState("");
-  const [artists, setArtists] = useState([]);
+//   const [token, setToken] = useState("");
+//   const [searchKey, setSearchKey] = useState("");
+//   const [artists, setArtists] = useState([]);
 
-  useEffect(() => {
-    const hash = window.location.hash;
-    let token = window.localStorage.getItem("token");
+//   useEffect(() => {
+//     const hash = window.location.hash;
+//     let token = window.localStorage.getItem("token");
 
-    // getToken()
+//     // getToken()
 
-    if (!token && hash) {
-      token = hash
-        .substring(1)
-        .split("&")
-        .find((elem) => elem.startsWith("access_token"))
-        .split("=")[1];
+//     if (!token && hash) {
+//       token = hash
+//         .substring(1)
+//         .split("&")
+//         .find((elem) => elem.startsWith("access_token"))
+//         .split("=")[1];
 
-      window.location.hash = "";
-      window.localStorage.setItem("token", token);
-    }
-
-    setToken(token);
-  }, []);
-
-  const logout = () => {
-    setToken("");
-    window.localStorage.removeItem("token");
-  };
-
-  const searchArtists = async (e) => {
-    e.preventDefault();
-    const { data } = await axios.get("https://api.spotify.com/v1/search", {
-      headers: {
-        Authorization: "Bearer ${token}",
-      },
-      params: {
-        q: searchKey,
-        type: "artist",
-      },
-    });
-
-    setArtists(data.artists.items);
-  };
-
-  const renderArtists = () => {
-    return artists.map((artist) => (
-      <div key={artist.id}>
-        {artist.images.length ? (
-          <img width={"100%"} src={artist.images[0].url} alt="" />
-        ) : (
-          <div>No Image</div>
-        )}
-        {artist.name}
-      </div>
-    ));
-  };
-
-  return (
-    <div className="app">
-      <div className="login-form">
-        <div className="form-head">
-          <img src={pic} alt="logo" />
-          <h1>Web Music</h1>
-        </div>
-        <div className="title">
-          {!token ? (
-            <a
-              href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-            >
-              Sign in
-            </a>
-          ) : (
-            <button onClick={logout}>Logout</button>
-          )}
-
-          {token ? (
-            <form onSubmit={searchArtists}>
-              <input
-                type="text"
-                onChange={(e) => setSearchKey(e.target.value)}
-              />
-              <button type={"submit"}>Search</button>
-            </form>
-          ) : (
-            <h2>Please login</h2>
-          )}
-
-          {renderArtists()}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Login;
-
-//   const Login = () => {
-//   const [errorMessages, setErrorMessages] = useState({});
-//   const [isSubmitted, setIsSubmitted] = useState(false);
-
-//   // User Login info
-//   const database = [
-//     {
-//       username: "user1",
-//       password: "pass1"
-//     },
-//     {
-//       username: "user2",
-//       password: "pass2"
+//       window.location.hash = "";
+//       window.localStorage.setItem("token", token);
 //     }
-//   ];
 
-//   const errors = {
-//     uname: "invalid username",
-//     pass: "invalid password"
+//     setToken(token);
+//   }, []);
+
+//   const logout = () => {
+//     setToken("");
+//     window.localStorage.removeItem("token");
 //   };
 
-//   const handleSubmit = (event) => {
-//     //Prevent page reload
-//     event.preventDefault();
+//   const searchArtists = async (e) => {
+//     e.preventDefault();
+//     const { data } = await axios.get("https://api.spotify.com/v1/search", {
+//       headers: {
+//         Authorization: "Bearer ${token}",
+//       },
+//       params: {
+//         q: searchKey,
+//         type: "artist",
+//       },
+//     });
 
-//     var { uname, pass } = document.forms[0];
-
-//     // Find user login info
-//     const userData = database.find((user) => user.username === uname.value);
-
-//     // Compare user info
-//     if (userData) {
-//       if (userData.password !== pass.value) {
-//         // Invalid password
-//         setErrorMessages({ name: "pass", message: errors.pass });
-//       } else {
-//         setIsSubmitted(true);
-//       }
-//     } else {
-//       // Username not found
-//       setErrorMessages({ name: "uname", message: errors.uname });
-//     }
+//     setArtists(data.artists.items);
 //   };
 
-//   // Generate JSX code for error message
-//   const renderErrorMessage = (name) =>
-//     name === errorMessages.name && (
-//       <div className="error">{errorMessages.message}</div>
-//     );
-
-//   // JSX code for login form
-//   const renderForm = (
-//     <div className="form">
-//       <form onSubmit={handleSubmit}>
-//         <div className="input-container">
-//           <label>Username </label>
-//           <input type="text" name="uname" required />
-//           {renderErrorMessage("uname")}
-//         </div>
-//         <div className="input-container">
-//           <label>Password </label>
-//           <input type="password" name="pass" required />
-//           {renderErrorMessage("pass")}
-//         </div>
-//         <div className="button-container">
-//           <input type="submit" />
-//         </div>
-//       </form>
-//     </div>
-//   );
+//   const renderArtists = () => {
+//     return artists.map((artist) => (
+//       <div key={artist.id}>
+//         {artist.images.length ? (
+//           <img width={"100%"} src={artist.images[0].url} alt="" />
+//         ) : (
+//           <div>No Image</div>
+//         )}
+//         {artist.name}
+//       </div>
+//     ));
+//   };
 
 //   return (
 //     <div className="app">
 //       <div className="login-form">
-//         <div className='form-head'>
-//         <img src={pic} alt='logo' />
-//          <h1>Web Music</h1>
+//         <div className="form-head">
+//           <img src={pic} alt="logo" />
+//           <h1>Web Music</h1>
 //         </div>
-         
-//         <div className="title">Sign In</div>
-//         {isSubmitted === true ? (<div>User is successfully logged in</div>) : renderForm}
+//         <div className="title">
+//           {!token ? (
+//             <a
+//               href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+//             >
+//               Sign in
+//             </a>
+//           ) : (
+//             <button onClick={logout}>Logout</button>
+//           )}
+
+//           {token ? (
+//             <form onSubmit={searchArtists}>
+//               <input
+//                 type="text"
+//                 onChange={(e) => setSearchKey(e.target.value)}
+//               />
+//               <button type={"submit"}>Search</button>
+//             </form>
+//           ) : (
+//             <h2>Please login</h2>
+//           )}
+
+//           {renderArtists()}
+//         </div>
 //       </div>
 //     </div>
 //   );
-// }
+// };
 
 // export default Login;
+
+  const Login = () => {
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const navigate = useNavigate();
+
+  // User Login info
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+        navigate("/music");
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+
+  return (
+    <div className="app">
+      <div className="login-form">
+        <div className='form-head'>
+        <img src={pic} alt='logo' />
+         <h1>Web Music</h1>
+        </div>
+         
+        <div className="title">Sign In</div>
+        {isSubmitted === true ? (<div> login Success</div>) : renderForm}
+      </div>
+    </div>
+  );
+}
+
+export default Login;
 
